cd41a69a634f42763ee2b4a634d8752921981cdc
Paulo Miguel <paulo2.0miguel@gmail.com>
2023-10-26 16:09:32 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function todo() {
+  return (
+    <div className="container">
+<h1>paulo</h1>
+<form>
+  <input type='text'>
+
+  </input>
+  <button>
+    Add
+  </button>
+</form>
+<ul>
+  <li>Item</li>
+</ul>
+    </div>
+  );
+}
+
+export default todo;
