53371ae605400ad9a13fc546986242f89cc1ddf4
Paulo Miguel <paulo2.0miguel@gmail.com>
2023-10-26 17:14:35 -0300

    aula-3

--- src/App.js
+++ src/App.js
@@ -7,28 +7,37 @@ import './App.css';
 function Todo() {
 
   const [text, setText] = useState("")
+  const [itens, setItems] = useState([])
+  function handlechange(event) {
+    let t = event.target.value;
+    setText(t)
+  }
 
-function handlechange(event){
- let t = event.target.value;
- setText(t)
-}
-
+  function AddItem(event) {
+    
+      event.preventDefault();
+      if (text) {
+      setItems([...itens, text])
+      setText("")
+    }
+  }
   return (
     <div className="container">
-<h1>paulo</h1>
-<form>
-  <input type='text' onChange={handlechange}>
+      <h1>paulo</h1>
+      <form>
+        <input type='text' value={text} onChange={handlechange}>
 
-  </input>
-  <button>
-    Add
-  </button>
-</form>
-<ul>
-  <li>{text}</li>
-</ul>
+        </input>
+        <button onClick={AddItem}>
+          Add
+        </button>
+      </form>
+      <ul>
+        <li>{itens.map(item => <li>{item}</li>)}</li>
+      </ul>
     </div>
   );
 }
 
 export default Todo;
+
